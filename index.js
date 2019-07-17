91118917f596e22269d6f10089e3f27d160fa6f5
glorious-genre-8033 <namontesino@gmail.com>
2019-07-17 21:00:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function theBeatlesPlay(musicians, instruments)
   
   for(var i = 0; i < musicians.length; i++)
   {
-    strings.push(musicians[i] + " plays " + intruments[i]);
+    strings.push(musicians[i] + " plays " + instruments[i]);
   }
   
   return strings;
